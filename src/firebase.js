85e7a3618fbb8f769fd126a9615177ede7a26b6b
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-12-22 12:50:20 +0000

    learning redux

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAtzV0xwccMb_MIWn1klOtG1s5TIkkdx6I",
+  authDomain: "linkedin-clone-f764c.firebaseapp.com",
+  projectId: "linkedin-clone-f764c",
+  storageBucket: "linkedin-clone-f764c.appspot.com",
+  messagingSenderId: "172437982413",
+  appId: "1:172437982413:web:0294289821784caebcad84",
+  measurementId: "G-5MF3Q7H2LK",
+};
