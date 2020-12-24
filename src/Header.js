24d132f83ecb721d0bdbe7397d67c194f2fc63c4
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-12-24 23:22:00 +0000

    header Section done

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,8 @@ import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 import "./Header.css";
 import HeaderOption from "./HeaderOption";
@@ -22,6 +24,14 @@ function Header() {
       </div>
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://avatars0.githubusercontent.com/u/40870043?s=460&u=797867b3813b54bdfd7834ce301100fe9205b484&v=4"
+          title="Me"
+        />
       </div>
     </div>
   );
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,12 @@
+import { Avatar } from "@material-ui/core";
 import React from "react";
 
 import "./HeaderOption.css";
-function HeaderOption({ Icon, title }) {
+function HeaderOption({ avatar, Icon, title }) {
   return (
     <div className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
+      {avatar && <Avatar src={avatar} className="headerOption__icon" />}
       <h3 className="headerOption__title">{title}</h3>
     </div>
   );
