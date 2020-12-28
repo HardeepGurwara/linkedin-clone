4f64dff46e9262e49db9bc89039df9f3823c0d4c
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-12-28 23:12:56 +0000

    designed the feed input container completely

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
---
+++ src/Feed.js
@@ -0,0 +1,35 @@
+import React from "react";
+import CreateIcon from "@material-ui/icons/Create";
+import "./Feed.css";
+import InputOption from "./InputOption";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photos" color="#70b5f9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#c0cbcd" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7fc15e"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
